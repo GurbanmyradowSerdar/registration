3be23fa4634354423a3de2b824aac34d5eb9d03b
Serdar <115923465+GurbanmyradowSerdar@users.noreply.github.com>
2023-11-03 22:57:49 +0500

    added assets

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Inter: ["Inter", "sans-serif"],
+        Roboto: ["Roboto", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
