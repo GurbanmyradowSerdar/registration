c8f9a02b1a3c8ec9807fe1c8c7a866a5b5b5da8c
Serdar <115923465+GurbanmyradowSerdar@users.noreply.github.com>
2023-11-04 23:16:43 +0500

    did 4 page content

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,21 @@ import { useState } from "react";
 import AsideTextComponent from "./components/AsideTextComponent";
 import InteractiveWrapperComponent from "./components/InteractiveWrapperComponent";
 import { textBlocks } from "./data";
-import ThirdPageComponent from "./components/pages/3-page";
+// import ThirdPageComponent from "./components/pages/3-page";
+import FourthPageComponent from "./components/pages/4-page";
 // import FirstPageContent from "./components/pages/1-page";
 // import SecondPageContent from "./components/pages/2-page";
 
 function App() {
-  const [pageIndex, setPageIndex] = useState(2);
+  const [pageIndex, setPageIndex] = useState(3);
   return (
     <main className="flex min-h-screen">
       <AsideTextComponent text={textBlocks[pageIndex]} />
       <InteractiveWrapperComponent pageIndex={pageIndex}>
         {/* <FirstPageContent /> */}
         {/* <SecondPageContent /> */}
-        <ThirdPageComponent />
+        {/* <ThirdPageComponent /> */}
+        <FourthPageComponent />
       </InteractiveWrapperComponent>
     </main>
   );
---
+++ src/components/pages/4-page/index.tsx
@@ -0,0 +1,80 @@
+import { useFormik } from "formik";
+import CustomInput from "../../common/CustomInput";
+import Divider from "../../common/Divider";
+import PrimaryButton from "../../common/buttons/PrimaryButton";
+import UnderPrimaryButton from "../../common/buttons/UnderPrimaryButton";
+import MainTitle from "../../common/text/MainTitle";
+import SubtitleTextComponent from "../../common/text/SubtitleTextComponent";
+
+const FourthPageComponent = () => {
+  const formik = useFormik({
+    initialValues: {
+      bvn: "",
+    },
+    validate: ({ bvn }) => {
+      let errors: { bvn?: string } = {};
+
+      if (bvn.length < 12) {
+        errors.bvn = "Required";
+      }
+
+      if (isNaN(Number(bvn))) {
+        errors.bvn = "Invalid number";
+      }
+
+      return errors;
+    },
+    onSubmit: ({ bvn }) => {
+      alert(`Bank verification number : ${JSON.stringify(bvn)}`);
+    },
+  });
+  return (
+    <div className="space-y-4 max-w-md mt-44">
+      <div className="space-y-3">
+        <MainTitle text="Complete Your Profile!" />
+        <SubtitleTextComponent text="For the purpose of industry regulation, your details are required." />
+        <Divider />
+      </div>
+      <form onSubmit={formik.handleSubmit} className="space-y-20">
+        <CustomInput
+          label="Bank verification number (BVN)"
+          name="bvn"
+          error={formik.errors.bvn}
+          anotherInput={
+            <div className="relative">
+              <input
+                maxLength={12}
+                name="bvn"
+                id="bvn"
+                type="tel"
+                placeholder="Enter BVN"
+                value={formik.values.bvn}
+                onChange={formik.handleChange}
+                className="w-full border border-bold-gray px-6 py-4 rounded-lg duration-300 transition-all 
+                placeholder:text-sm focus-within:outline focus-within:outline-primary-color focus-within:shadow-xl"
+              />
+              <img
+                src="./check.svg"
+                alt="check"
+                className="w-6 h-6 absolute top-2/4 right-6
+                opacity-0  transition-all duration-300"
+                style={{
+                  opacity: formik.values.bvn.length == 12 ? 1 : 0,
+                  transform: `translateY(-50%) ${
+                    formik.values.bvn.length == 12 ? " scale(1)" : "scale(0.5)"
+                  }`,
+                }}
+              />
+            </div>
+          }
+        />
+        <div className="pt-3 space-y-4">
+          <PrimaryButton text="Save & Continue" />
+          <UnderPrimaryButton />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default FourthPageComponent;
